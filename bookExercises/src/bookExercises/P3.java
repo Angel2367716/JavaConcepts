15a9d29de9e7da27b66f2a3d02efdbca13a79537
Marco Angel <angel2367716@gmail.com>
2020-10-14 21:46:46 -0700

    basic user input

--- bookExercises/src/bookExercises/P3.java
+++ bookExercises/src/bookExercises/P3.java
@@ -1,10 +1,12 @@
 package bookExercises;
 
+//Show the message "Hello, name"
+
 public class P3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
+		System.out.println("Hello, cat");
 	}
 
 }
