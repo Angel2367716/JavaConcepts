226c10d134f51c3038311db792c97ed6d8f3abae
Marco Angel <angel2367716@gmail.com>
2020-10-14 20:54:09 -0700

    updated 2 basic java excercises

---
+++ module1/src/module1/P1.java
@@ -0,0 +1,15 @@
+package module1;
+
+/*
+Write a program that prints a greeting of your choice,
+perhaps in a language other than English.
+*/
+
+public class P1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+
+	}
+
+}
