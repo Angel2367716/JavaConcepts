4ee77ac964dfa30b53ad1ac26c71ab77b18957a8
Marco Angel <angel2367716@gmail.com>
2020-10-15 19:19:37 -0700

    Added java truck drwaing class

---
+++ bookExercises/src/bookExercises/TruckDrawing.java
@@ -0,0 +1,14 @@
+package bookExercises;
+
+public class TruckDrawing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("   /-+-----------+");
+		System.out.println("+~: |            |");
+		System.out.println("+---+------------+");
+		System.out.println("   o   oo     o   ");
+
+	}
+
+}
