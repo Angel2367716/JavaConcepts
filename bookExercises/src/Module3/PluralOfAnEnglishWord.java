cdcdf0629a1d6882568c6acc46796a912556c3d5
Marco Angel <angel2367716@gmail.com>
2020-10-21 21:21:22 -0700

    Book example does not go over incompatible operand type char and string on the plural of an English word demo

---
+++ bookExercises/src/Module3/PluralOfAnEnglishWord.java
@@ -0,0 +1,28 @@
+package Module3;
+
+import java.util.Scanner;
+
+public class PluralOfAnEnglishWord {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String word = in.next();
+		int lastPosition = word.length() - 1;
+		char last = word.charAt(lastPosition);
+		char before = word.charAt(lastPosition - 1);
+		String plural;
+
+		if (word.endsWith("ch") || word.endsWith("s") || word.endsWith("sh") || word.endsWith("x")
+				|| word.endsWith("z")) {
+			// line 15
+			plural = word + "es";
+		} else if (!(before == "a" || before == "e" || before == "i" || before == "o" || before == "u")
+				&& last == "y") {
+			// line 19
+			plural = word.substring(0, lastPosition) + "ies";
+		} else {
+			plural = word + "s";
+		}
+	}
+
+}
