557699d51846a1410f74ba5320a8f1799d001ba4
Marco Angel <angel2367716@gmail.com>
2020-10-31 19:33:36 -0700

    moved main method to the bottom

--- bookExercises/src/Module6/Sec06_ReadTime.java
+++ bookExercises/src/Module6/Sec06_ReadTime.java
@@ -4,17 +4,10 @@ import java.util.Scanner;
 
 public class Sec06_ReadTime {
 
-	public static void main(String[] args) {
-		System.out.println("Please enter a time: hours, then minutes.");
-		int hours = readInBetween(0, 23);
-		int minutes = readInBetween(0, 59);
-		System.out.printf("you entered %d hours and %d minutes. %n", hours, minutes);
-	}
-
 	/**
 	 * Prompts a suer to enter a value within a given range until the user provides
 	 * a valid input
-	 * 
+	 *
 	 * @param low  the smallest allowable input
 	 * @param high the largest allowable input
 	 * @return the value provided by the user (between low and high, inclusive)
@@ -27,6 +20,13 @@ public class Sec06_ReadTime {
 			input = in.nextInt();
 		} while (input < low || input > high);
 		return input;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Please enter a time: hours, then minutes.");
+		int hours = readInBetween(0, 23);
+		int minutes = readInBetween(0, 59);
+		System.out.printf("you entered %d hours and %d minutes. %n", hours, minutes);
 	}
 
 }
