7428c82146ae7158d7a60c46f216fbb06dcdd570
Marco Angel <angel2367716@gmail.com>
2020-10-31 20:18:01 -0700

    completed method simplification

--- bookExercises/src/Module6/SimplifyingInputs.java
+++ bookExercises/src/Module6/SimplifyingInputs.java
@@ -1,10 +1,21 @@
 package Module6;
 
+import java.util.Scanner;
+
 public class SimplifyingInputs {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double price = getInput("First item");
+		price = price + getInput("Second item");
+		double rate = getInput("Tax rate in percent ");
+		double tax = price * rate / 100;
+		System.out.println("Ammount due: " + (price + tax));
+	}
 
+	public static double getInput(String prompt) {
+		Scanner in = new Scanner(System.in);
+		System.out.println(prompt + ": ");
+		return in.nextDouble();
 	}
 
 }
